e96798574e8f0491938acf72be4cab62f2a51e6d
Ryan Davis <rd11490@gmail.com>
2015-03-30 20:53:01 -0400

    Initial commit

---
+++ Scrape_espn_league.py
@@ -0,0 +1,22 @@
+__author__ = 'Ryan'
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import pickle
+import mechanize
+
+def loginToESPN():
+    pass
+
+def scrapePlayerProjections():
+    pass
+
+def scrapeTeamPlayers():
+    pass
+
+def scrapeMatchups():
+    pass
+
+def scrapeMatchupPlayers():
+    pass
